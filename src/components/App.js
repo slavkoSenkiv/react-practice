6bba57bc4443e05b67c807c6ad2b412c66b63e31
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-06-10 17:18:33 +0300

    ternary + and operator basic practice

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Form from "./Form";
 
-const isLoggedIn = false;
+const currentTime = new Date().getHours();
+
 
 function App() {
-  return <div className="container">{isLoggedIn ? <h1>Hello</h1> : <Form />}</div>;
+  //return <div className="container">{currentTime > 18 ? <h1>Why are you still working?</h1> : <h1>How long are you going to work?</h1>}</div>;
+  return <div className="container">{currentTime > 12 && <h1>Why are you still working?</h1>}</div>;
 }
 
 export default App;
