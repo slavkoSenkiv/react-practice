35c09b494d94db7e1d526c18f07f4ee720cfb327
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-06-10 18:31:03 +0300

    final code

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Form from "./Form";
 
-const currentTime = new Date().getHours();
-
-
+//const newUser = false;
+const existingUser = false;
+git 
 function App() {
-  //return <div className="container">{currentTime > 18 ? <h1>Why are you still working?</h1> : <h1>How long are you going to work?</h1>}</div>;
-  return <div className="container">{currentTime > 12 && <h1>Why are you still working?</h1>}</div>;
+  return (
+    <div className="container">
+      {/* {<Form userStatus={newUser}/>} */}
+      {<Form userStatus={existingUser}/>}
+    </div>
+  )
 }
 
 export default App;
