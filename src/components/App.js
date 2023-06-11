6575b6638053d91248587d173af5a6ed2b0ed379
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-06-11 08:28:36 +0300

    usestate playground

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
-  return <h1>Hello</h1>
+  //let count = 0;
+  const state = useState(321);
+
+  function increase(){
+    //count++;
+    //console.log('I got clicked, count = ', count);
+  }
+
+  return (
+    <div className="container">
+      <h1>{state[0]}</h1>
+      <button onClick={increase}>+</button>
+    </div>
+  )
+    
 }
 
 export default App;
