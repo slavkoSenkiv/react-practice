23f425d15f1b5c159bb32bf8562e69805156e3eb
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-06-12 09:55:27 +0300

    udemy solution to upd heading adter submit button click

--- src/components/App.js
+++ src/components/App.js
@@ -3,24 +3,26 @@ import React, { useState } from "react";
 function App() {
 
   const [name, setName] = useState("");
+  const [headingText, setHeadingText] = useState("");
 
   function handleChange(event){
-    //console.log('changed');
     console.log(event.target.value);
-    setName(inputValue);
+    setName(event.target.value);
   }
 
-  const inputValue = document.getElementsByTagName('input')[0].value;
-
+  function handleClick(){
+    setHeadingText(name);
+  }
   return (
     <div className="container">
-      <h1>Hello {name}</h1>
-      <input 
-        //onChange={handleChange}
-        //value={name}
-        type="text" 
-        placeholder="What's your name?" />
-      <button onClick={handleChange}>Submit</button>
+      <h1>Hello {headingText}</h1>
+        <input 
+          onChange={handleChange}
+          type="text" 
+          placeholder="What's your name?" />
+        <button onClick={handleClick}>
+        Submit
+        </button>
     </div>
   );
 };
