f41443d870d1b239d27d752584c6e3424b0423c6
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-06-12 08:05:09 +0300

    my solution to update heading after submit button click

--- src/components/App.js
+++ src/components/App.js
@@ -7,18 +7,20 @@ function App() {
   function handleChange(event){
     //console.log('changed');
     console.log(event.target.value);
-    setName(event.target.value);
+    setName(inputValue);
   }
+
+  const inputValue = document.getElementsByTagName('input')[0].value;
 
   return (
     <div className="container">
       <h1>Hello {name}</h1>
       <input 
-        onChange={handleChange}
-        value={name}
+        //onChange={handleChange}
+        //value={name}
         type="text" 
         placeholder="What's your name?" />
-      <button>Submit</button>
+      <button onClick={handleChange}>Submit</button>
     </div>
   );
 };
