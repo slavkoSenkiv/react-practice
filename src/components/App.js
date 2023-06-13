db08d529581426cd59621d8d78e96aaaa88ebb4c
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-06-13 08:44:09 +0300

    v2

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,50 @@
 import React, {useState} from "react";
 
 function App() {
-  const [fName, setFName] = useState("");
-  const [lName, setLName] = useState("");
 
-  function updFName(event){
-    const firstName = event.target.value;
-    setFName(firstName);
-  }
-  function updLName(event){
-    const lastName = event.target.value;
-    setLName(lastName);
-  }
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: ""
+  });
 
+  function handleChange(event){
+    const newValue = event.target.value;
+    const inputName = event.target.name;
+
+    setFullName(prevValue =>{
+      if (inputName === 'fName'){
+        return {
+          fName: newValue,
+          lName: prevValue.lName
+        };
+      } else if (inputName === 'lName'){
+        return {
+          fName: prevValue.fName,
+          lName: newValue
+        }
+      }
+    })
+  }
 
   return (
     <div className="container">
-      <h1>Hello {fName} {lName}</h1>
+      <h1>Hello {fullName.fName} {fullName.lName}</h1>
       <form>
 
         <input 
           placeholder="First Name" 
           name="fName" 
-          onChange={updFName} 
-          value={fName} />
+          onChange={handleChange} 
+          value={fullName.fName} 
+          />
 
         <input 
           placeholder="Last Name" 
           name="lName" 
-          onChange={updLName} 
-          value={lName} />
-          
+          onChange={handleChange} 
+          value={fullName.lName} 
+          />
+
         <button>Submit</button>
       </form>
     </div>
