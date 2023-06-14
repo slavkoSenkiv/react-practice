53176361ef0a8c2bcb9877200e24e4b9dd2efd19
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-06-14 21:20:23 +0300

    keep root branch clean

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="container">
-      <div className="heading">
-        <h1>To-Do List</h1>
-      </div>
-      <div className="form">
-        <input type="text" />
-        <button>
-          <span>Add</span>
-        </button>
-      </div>
-      <div>
-        <ul>
-          <li>A Item</li>
-        </ul>
-      </div>
-    </div>
-  );
+  return <h1>HELLO</h1>
 }
 
 export default App;
