994ebc791519104ed85fc7496b661b8b081a9a6a
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-06-14 07:59:07 +0300

    made code but still getting an error

--- src/components/App.js
+++ src/components/App.js
@@ -7,17 +7,61 @@ function App() {
     email: ""
   });
 
+  function handleChange(e){
+    const [value, name] = e.target;
+
+    setContact(prevValue =>{
+      if (name === 'fName') {
+        return {
+          fName : value,
+          lName : prevValue.value,
+          email : prevValue.value
+        };
+      } else if (name === 'lName') {
+        return {
+          fName : prevValue.value,
+          lName : value,
+          email : prevValue.value
+        };
+      } else if (name === 'email') {
+        return {
+          fName : prevValue.value,
+          lName : prevValue.value,
+          email : value
+        };
+      }
+    });
+  }
+
   return (
     <div className="container">
-      <h1>
-        Hello {contact.fName} {contact.lName}
-      </h1>
+      <h1>Hello {contact.fName} {contact.lName}</h1>
       <p>{contact.email}</p>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
+
+        <input 
+          name="fName" 
+          placeholder="First Name"
+          onChange={handleChange} 
+          value={contact.fName}
+        />
+
+        <input 
+          name="lName" 
+          placeholder="Last Name" 
+          onChange={handleChange}
+          value={contact.lName}
+        />
+
+        <input 
+          name="email" 
+          placeholder="Email" 
+          onChange={handleChange}
+          value={contact.email}
+        />
+
         <button>Submit</button>
+
       </form>
     </div>
   );
