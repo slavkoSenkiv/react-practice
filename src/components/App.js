6c4faf812a9b0c9e5c15b3d40a14d1875c2deb1e
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-06-15 09:17:45 +0300

    my repetition

--- src/components/App.js
+++ src/components/App.js
@@ -7,42 +7,38 @@ function App() {
   const [newArr, setNewArr] = useState(currArr);
   const [newTask, setNewTask] = useState("");
 
-  function addToList(){
-    setNewArr(prevArr=>[newTask, ...prevArr]);
-    setNewTask("");
+  function handleInput(e){
+    setNewTask(e.target.value);
   }
 
-  function handleInputChange(event){
-    setNewTask(event.target.value);
+  function handleItemSave(){
+    setNewArr([...newArr, newTask]);
+    setNewTask("")
   }
 
   return (
     <div className="container">
 
-      <div className="heading">
-        <h1>To-Do List</h1>
-      </div>
+      <div className="heading"><h1>To-Do List</h1></div>
 
       <div className="form">
 
         <input 
-          type="text" 
-          name="newTask" 
+          type="text"
+          name='newTask'
           value={newTask}
-          onChange={handleInputChange}
+          onChange={handleInput}
         />
 
         <button>
-          <span onClick={addToList}>Add</span>
+          <span onClick={handleItemSave}>Add</span>
         </button>
 
       </div>
 
       <div>
         <ul>
-          {newArr.map((task, index) => (
-            <li key={index}>{task}</li>
-          ))};
+          {newArr.map((task, index)=>(<li key={index}>{task}</li>))};
         </ul>
       </div>
 
