182bd4370578c5383a95a56df48ef0993652ace0
Yaroslav Senkiv <slav.senkiv@bytro.com>
2023-06-15 08:36:52 +0300

    done the task with help of gpt

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+
+  const [newArr, updArr] = useState([1, 2]);
+  const [newTask, setNewTask] = useState("");
+
+  function addToList(){
+    updArr(prevArr=>[...prevArr, newTask]);
+    setNewTask("");
+  }
+
+  function handleInputChange(event){
+    setNewTask(event.target.value);
+  }
+
   return (
     <div className="container">
+
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
+
       <div className="form">
-        <input type="text" />
+
+        <input 
+          type="text" 
+          name="newTask" 
+          value={newTask}
+          onChange={handleInputChange}
+        />
+
         <button>
-          <span>Add</span>
+          <span onClick={addToList}>Add</span>
         </button>
+
       </div>
+
       <div>
         <ul>
-          <li>A Item</li>
+          {newArr.map((task, index) => (
+            <li key={index}>{task}</li>
+          ))};
         </ul>
       </div>
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ root.render(
   </React.StrictMode>
 );
 
-
 //CHALLENGE: Make this app work by applying what you've learnt.
 //1. When new text is written into the input, its state should be saved.
 //2. When the add button is pressed, the current data in the input should be
